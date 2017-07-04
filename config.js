88ed6864cc6fe5a5ac645b3669e5fa06f0634c8d
yangjun <yangjun@wepiao.com>
2017-07-04 19:52:09 +0800

    demo

---
+++ config.js
@@ -0,0 +1,13 @@
+var ShaLuDatas = [{
+	name: '垚哥',
+	url: 'http://share.jl.ztgame.com/1/6/ad1bd982a4bc11e697c35254006a74ca'
+}, {
+	name: '盛宴',
+	url: 'http://share.jl.ztgame.com/1/6/885943c2023c11e7a0135254006a74ca'
+}, {
+	name: '李晓旭',
+	url: 'http://share.jl.ztgame.com/1/6/1d6276b0a14d11e6b2b05254006a74ca'
+}, {
+	name: '麦兜',
+	url: 'http://share.jl.ztgame.com/1/6/d7d73568adae11e6bf2f5254006a74ca'
+}]
