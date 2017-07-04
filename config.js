11a5b48020aeeb00771afe246513978a1f4b86af
yangjun <yangjun@wepiao.com>
2017-07-04 21:41:47 +0800

    曲阿

--- config.js
+++ config.js
@@ -1,19 +1,23 @@
+var ShaLuBase = 'http://share.jl.ztgame.com/1/6/'
 var ShaLuDatas = [{
 	name: '垚哥',
-	url: 'http://share.jl.ztgame.com/1/6/ad1bd982a4bc11e697c35254006a74ca'
+	uid: 'ad1bd982a4bc11e697c35254006a74ca'
 }, {
 	name: '盛宴',
-	url: 'http://share.jl.ztgame.com/1/6/885943c2023c11e7a0135254006a74ca'
+	uid: '885943c2023c11e7a0135254006a74ca'
 }, {
 	name: '李晓旭',
-	url: 'http://share.jl.ztgame.com/1/6/1d6276b0a14d11e6b2b05254006a74ca'
+	uid: '1d6276b0a14d11e6b2b05254006a74ca'
 }, {
 	name: '麦兜',
-	url: 'http://share.jl.ztgame.com/1/6/d7d73568adae11e6bf2f5254006a74ca'
+	uid: 'd7d73568adae11e6bf2f5254006a74ca'
 }, {
 	name: '射手小号',
-	url: 'http://share.jl.ztgame.com/1/6/79adcd38bf6211e6aa1a5254006a74ca'
+	uid: '79adcd38bf6211e6aa1a5254006a74ca'
 }, {
 	name: '射手大号',
-	url: 'http://share.jl.ztgame.com/1/6/1573fef4ea7911e6bec45254006a74ca'
+	uid: '1573fef4ea7911e6bec45254006a74ca'
+}, {
+	name: '曲阿',
+	uid: '52101ccaabd711e68ce95254006a74ca'
 }]
